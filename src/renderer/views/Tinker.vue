ced54dedd3320d3d4229ee16835dbb97a0650e44
tmdh <tarequemd.hanif@yahoo.com>
2021-03-04 19:38:45 +0300

    Fix multiple tinker output

--- src/renderer/views/Tinker.vue
+++ src/renderer/views/Tinker.vue
@@ -30,9 +30,25 @@ export default {
     };
   },
   computed: {
-    ...mapState(["dir", "code", "output"]),
+    ...mapState(["dir"]),
     theme() {
       return this.$store.state.dark && this.$store.state.licensed ? "dracula" : "one-light";
+    },
+    code: {
+      set(value) {
+        this.$store.state.code = value;
+      },
+      get() {
+        return this.$store.state.code;
+      }
+    },
+    output: {
+      set(value) {
+        this.$store.state.output = value;
+      },
+      get() {
+        return this.$store.state.output;
+      }
     }
   },
   methods: {
@@ -40,8 +56,9 @@ export default {
       this.$store.state.tinkering = true;
       const tinker = spawn("php", ["artisan", "tinker"], { cwd: this.dir });
       tinker.stdout.setEncoding("utf-8");
+      this.$store.state.output = "";
       tinker.stdout.on("data", (data) => {
-        this.$store.state.output = data;
+        this.$store.state.output += data;
         this.$store.state.tinkering = false;
       });
       tinker.stdin.write(this.code);
