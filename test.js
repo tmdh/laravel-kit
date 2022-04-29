99df5bdaed195361285ed5dd02747986cb842588
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2022-04-29 16:05:48 +0600

    Add tinker.php test

---
+++ test.js
@@ -0,0 +1,10 @@
+const { execSync } = require("child_process");
+execSync(`composer create-project laravel/laravel ${require("os").tmpdir}/ltest`);
+try {
+  let a = execSync(`php src/main/tinker.php ${require("os").tmpdir}/ltest 'use Illuminate\\Foundation\\Inspiring;Inspiring::quote();'`);
+  console.log(a);
+} catch (e) {
+  console.error("Error in tinker.php");
+  console.error(e.stdout.toString());
+  process.exit(1);
+}
