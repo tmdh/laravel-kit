482737d16eaba41a88c3cd4c6f00d0025ca0ae12
Tareque Md Hanif <tarequemd.hanif@yahoo.com>
2022-09-07 22:55:43 +0600

    Add php executable path for Windows in GH actions

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
+const php_path = () => {
+  if (process.platform == "win32" && process.env.CI == "true") {
+    return "c:\\tools\\php\\php.exe";
+  }
+  return "php";
+}
 const { execSync } = require("child_process");
 execSync(`composer create-project laravel/laravel ${require("os").tmpdir}/ltest`);
 try {
-  let a = execSync(`php src/main/tinker.php ${require("os").tmpdir}/ltest 'use Illuminate\\Foundation\\Inspiring;Inspiring::quote();'`);
+  let a = execSync(`${php_path()} src/main/tinker.php ${require("os").tmpdir}/ltest 'use Illuminate\\Foundation\\Inspiring;Inspiring::quote();'`);
   console.log("Here is an inspiring quote:");
   console.log(a.toString());
 } catch (e) {
